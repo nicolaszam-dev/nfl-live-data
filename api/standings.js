bff9fa8980fffa1acd0b3513f363e1d087307dc5
nicolaszam-dev <zambranonicolas560@gmail.com>
2026-07-27 19:21:21 -0600

    Update standings.js

--- api/standings.js
+++ api/standings.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://site.api.espn.com/apis/site/v2/sports/football/nfl/standings');
+    const response = await fetch('https://cdn.espn.com/core/nfl/standings?xhr=1');
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
